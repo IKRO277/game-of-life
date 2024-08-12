a02270d7ac5a1a7771d03688c341a3c298f0f6f4
Icaro Pereira De Paula <136732875+IKRO277@users.noreply.github.com>
2024-08-12 17:48:49 -0400

    Update Main.java 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-
-
 import ui.UI;
 
 import java.util.Locale;
@@ -9,12 +7,10 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
         UI ui = new UI();
-
         ui.addChessParams(args);
+        ui.printInstructions(sc);
         ui.runChess(ui.generateChess(args));
-
         sc.close();
     }
 }
