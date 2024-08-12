488118e5b6714d0aefdabb54d4c40546b6c20096
Icaro Pereira De Paula <136732875+IKRO277@users.noreply.github.com>
2024-08-12 17:47:06 -0400

    Update Cell.java 3

--- src/entities/Cell.java
+++ src/entities/Cell.java
@@ -14,41 +14,33 @@ public class Cell {
         this.chess = chess;
     }
 
-
     public Boolean getLife() {
         return life;
     }
-
 
     public void setLife(Boolean life) {
         this.life = life;
     }
 
-
     public Cell[][] getChess() {
         return chess;
     }
-
 
     public void setChess(Cell[][] chess) {
         this.chess = chess;
     }
 
-
     public Integer getRow() {
         return row;
     }
-
 
     public void setRow(Integer row) {
         this.row = row;
     }
 
-
     public Integer getCol() {
         return col;
     }
-
 
     public void setCol(Integer col) {
         this.col = col;
